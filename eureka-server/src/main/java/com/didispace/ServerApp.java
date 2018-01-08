edc7b25114e6220a107203839d755a49ab1914ed
yeol_u <1300717253@qq.com>
2018-01-08 11:09:44 +0800

    注册中心

---
+++ eureka-server/src/main/java/com/didispace/ServerApp.java
@@ -0,0 +1,14 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApp.class, args);
+	}
+
+}
